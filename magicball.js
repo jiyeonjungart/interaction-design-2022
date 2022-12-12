e12daa0bb1dcad8b36a718a68cbccd2dfb3bc249
jiyeonjungart <jiyeonjungart@gmail.com>
2022-12-12 01:02:55 -0500

    magic ball js

--- magicball.js
+++ magicball.js
@@ -1,3 +1,19 @@
-function myFunction() {
-    document.getElementsByClassName("magicballback").style.display = "block";
+const back = document.querySelector(".magicballback");
+const front = document.querySelector(".magicballfront");
+
+let displayed = "front";
+
+front.addEventListener("click", () => {
+  if (displayed === "front") {
+    back.style.display = "block";
+    front.style.display = "none";
+    displayed = "back";
   }
+});
+back.addEventListener("click", () => {
+  if (displayed === "back") {
+    front.style.display = "block";
+    back.style.display = "none";
+    displayed = "front";
+  }
+});
